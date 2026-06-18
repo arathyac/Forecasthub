1466ece07c21534fc8a0ca9793a99d4159060f6b
Ubuntu <ubuntu@ip-172-31-84-180.ec2.internal>
2026-06-18 12:28:12 +0000

    Add dynamic weather based UI

--- script.js
+++ script.js
@@ -101,3 +101,22 @@ function changeBackground(weather) {
         document.body.classList.add("default");
     }
 }
+setInterval(() => {
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+
+                const url =
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+                fetchWeather(url);
+            }
+        );
+    }
+
+}, 60000);
